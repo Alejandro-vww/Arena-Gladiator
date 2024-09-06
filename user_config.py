1218e4c2d11d06ef92289e12a4645ce4cf57b7d7
Alejandro-vww <alejandro.rguez.cos@gmail.com>
2024-09-07 01:28:57 +0200

    Bot with default mode working

--- user_config.py
+++ user_config.py
@@ -1,3 +1,4 @@
-username = 'Dienekes'
-log_path = 'C:\\Users\\TONTO\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\Player.log'
+import os
+
+log_path = os.path.join(os.environ['USERPROFILE'], 'AppData', 'LocalLow', 'Wizards Of The Coast', 'MTGA', 'Player.log')
 min_afk_time = 10
